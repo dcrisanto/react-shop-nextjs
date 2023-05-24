56e322acf6ddcfc076fb50074a5b4ab1bf91f787
dcrisanto <dorelly.crisanto@gmail.com>
2023-05-24 18:39:06 -0500

    pwa

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['picsum.photos', 'imgv3.fotor.com', 'placeimg.com', 'encrypted-tbn2.gstatic.com', 'api.lorem.space', 'api.escuelajs.co', 'm.media-amazon.com', 'loremflickr.com'],
+    domains: ['picsum.photos', 'imgv3.fotor.com', 'placeimg.com', 'encrypted-tbn2.gstatic.com', 'api.lorem.space', 'api.escuelajs.co', 'm.media-amazon.com', 'loremflickr.com', 'i.ytimg.com', 'images.pexels.com'],
   }
-}
+};
 
-module.exports = nextConfig
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development'
+})
 
+module.exports = withPWA(nextConfig);
--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import ProductList from '@containers/ProductList';
 export const metadata = {
   title: 'PlatziStore Nextjs',
   description: 'Migración de React a Nextjs',
+  manifest: '/manifest.json',
 };
 
 export default function Home() {
